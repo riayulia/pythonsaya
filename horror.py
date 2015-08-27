3d3f67cc84f4f541080142f6a4012216d0927b93
riayulia <riayuliaya@gmail.com>
2015-08-27 11:09:05 +0700

    first commit

---
+++ horror.py
@@ -0,0 +1,26 @@
+print ("Yuk main pilih pintu")
+pilih=input("Pilih 1 atau 2 ?")
+if pilih==1:
+    print(" 1.Harta")
+    print(" 2.Tahta")
+    print(" 3.Wanita")
+    pilih2=input("Pilih 1/2/3 ? ")
+    if pilih2==1:
+        print("Anda Kaya")
+        print("Selesai")
+    elif pilih2==2:
+        print("Anda Raja")
+        print("Selesai")
+    else:
+        print("Anda Menderita")
+        print("Selesai")
+else:
+    print(" 1.Homo")
+    print(" 2.Banci")
+    pilih3=input("Pilih 1/2 ? ")
+    if pilih3==1:
+        print("Anda dimakan")
+        print("Selesai")
+    else:
+        print("Anda ikutan")  
+        print("Selesai")      
---
+++ loop.py
@@ -0,0 +1,6 @@
+#deret input
+bil=input("masukkan bilangan maksimal deret:")
+a=bil-1
+while bil<a:
+	a=bil-1
+	print a,
